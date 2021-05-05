ee7e05a9cf37ac89e390a85b7a5a42129d3307bc
Amiel Martin <amiel.martin@gmail.com>
2021-05-05 14:41:18 -0800

    Generate gem with `bundle gem action_sprout-elastic_search`

---
+++ lib/action_sprout/elastic_search.rb
@@ -0,0 +1,8 @@
+require "action_sprout/elastic_search/version"
+
+module ActionSprout
+  module ElasticSearch
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/action_sprout/elastic_search/version.rb
@@ -0,0 +1,5 @@
+module ActionSprout
+  module ElasticSearch
+    VERSION = "0.1.0"
+  end
+end
